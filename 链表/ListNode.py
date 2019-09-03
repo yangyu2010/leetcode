47c75a3ab8d89e98addbbb6e8c14cd827baaea4a
yangyu2010 <yangyu2010@aliyun.com>
2019-09-03 14:29:19 +0800

    更新

--- 链表/ListNode.py
+++ 链表/ListNode.py
@@ -6,3 +6,19 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+        self.prev = None
+
+    def __str__(self):
+        s = ''
+        if self.prev:
+            s += str(self.prev.val)
+        else:
+            s += 'none'
+
+        s += ('->%d->' % self.val)
+
+        if self.next:
+            s += str(self.next.val)
+        else:
+            s += 'none'
+        return s
